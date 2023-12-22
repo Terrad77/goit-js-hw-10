f895a1c22662708601d832155d420a6df778345c
Dmytro Terlovoy <terlovoy77@gmail.com>
2023-12-22 19:49:47 +0200

    JS done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,56 +2,46 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
-const delayInput = form.querySelector('.js-delay');
 
-let settled;
-
-form.addEventListener('click', event => {
-  if (event.target.type === 'radio') {
-    settled = event.target.value;
-  }
-});
-
-form.addEventListener('submit', event => {
+form.addEventListener('submit', function (event) {
   event.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stateSelected = document.querySelector('input[name="state"]:checked');
+
   const delay = delayInput.value;
+  const state = stateSelected ? stateSelected.value : null;
 
-  switch (settled) {
-    case 'fulfilled':
-      resolve(delay);
-      break;
-    case 'rejected':
-      reject(delay);
-      break;
-  }
+  const promise = new Promise((resolve, reject) => {
+    if (state === 'fulfilled') {
+      setTimeout(() => {
+        resolve(`Fulfilled promise in ${delay}ms`);
+      }, delay);
+    } else if (state === 'rejected') {
+      setTimeout(() => {
+        reject(`Rejected promise in ${delay}ms`);
+      }, delay);
+    } else {
+      reject('Invalid state');
+    }
+    delayInput.value = '';
+  });
+
+  promise
+    .then(result => {
+      iziToast.show({
+        message: `✅ ${result}`,
+        color: 'green',
+        position: 'topRight',
+        close: false,
+      });
+    })
+    .catch(error => {
+      iziToast.show({
+        message: `❌ ${error}`,
+        color: 'red',
+        position: 'topRight',
+        close: false,
+      });
+    });
 });
-
-function resolve(delay) {
-  return Promise.resolve(showResolve(delay));
-}
-
-function showResolve(delay) {
-  setTimeout(() => {
-    iziToast.show({
-      message: `✅ Fulfilled promise in ${delay}ms`,
-      color: 'green',
-      position: 'topRight',
-      close: false,
-    });
-  }, delay);
-}
-
-function reject(delay) {
-  return Promise.reject(showReject(delay));
-}
-
-function showReject(delay) {
-  setTimeout(() => {
-    iziToast.show({
-      message: `❌ Rejected promise in ${delay}ms`,
-      color: 'red',
-      position: 'topRight',
-      close: false,
-    });
-  }, delay);
-}
